1df5dfc77ba7ace1f0549a5fdababc98a8deb9c9
arlen <arlenismailov02@gmail.com>
2024-07-03 13:26:16 +0600

    3_lesson

--- test/test_main.py
+++ test/test_main.py
@@ -1,9 +1,10 @@
-from src.main import A
+def test_sum():
+    x = 1
+    y = 2
+    assert x + y == 3
 
 
-def test_main():
-    assert A.x == 1
-
-
-def test_2():
-    assert 2 == 2
+def test_division():
+    x = 1
+    y = 2
+    assert x / y == 0.5
